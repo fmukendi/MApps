aa6c33fe104e1ca8d0c6e65f08f6baebbb6f283d
fmukendi <franck.mukendi@gmail.com>
2016-01-04 22:56:12 -0800

    Basic Knowledge

--- MukeApps/server.js
+++ MukeApps/server.js
@@ -1,6 +1,36 @@
-﻿var http = require('http');
+﻿
+////////////////////////////////////
+// PART I - LIBRARIES(Modules)
+///////////////////////////////////
+
+//1.1 we need http module to create a server. Http is standard Node.js Library.
+var http = require('http');
+
+
+///////////////////////////////////////////
+// RUN SERVER 
+//////////////////////////////////////////
+
+//1.1 Create the server 
+var server = http.createServer(function (req, res) {
+    
+    var body = req.url;
+    var html = "<html><body><h1>" + body + "</h1></body></html>";
+
+    console.log(req.url);
+    res.write(html);
+    res.end();
+});
+
+//1.2 Listen to the Server
+
+server.listen(3000);
+
+
+/*var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
+ */
